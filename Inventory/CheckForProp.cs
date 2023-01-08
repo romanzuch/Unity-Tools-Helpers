3b6d544541fd26a5042465bb19f1d943bcc6be63
Roman <42408195+romanzuch@users.noreply.github.com>
2023-01-08 17:42:13 +0100

    extended code to reflect diagram

--- Inventory/CheckForProp.cs
+++ Inventory/CheckForProp.cs
@@ -3,8 +3,20 @@ private void CheckForPropInNextField()
     Ray ray = new Ray(transform.position, playerLookDirection);
     RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, rayLength, layerMask);
     if (hit.collider != null) {
-        // do something
-    } else {
-        // do something else
+        if (hit.transform.name == "Chest" || hit.transform.tag == "Container")
+            {
+                ContainerController containerController = hit.transform.GetComponent<ContainerController>();
+                if (containerController != null)
+                {
+                    containerController.ShowContainerIndicator();
+                    previousContainerController = containerController;
+                    containerController.InteractWithContainer();
+                }
+            }
+    } 
+    else if (hit.collider == null && previousContainerController != null) 
+    {
+        previousContainerController.HideContainerIndicator();
+        previousContainerController = null;
     }
 }
